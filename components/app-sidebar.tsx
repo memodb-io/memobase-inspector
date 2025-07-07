090461ec5ddf43095ef8b1e3b43af857ac5ac76c
Gener <435669237@qq.com>
2025-07-07 12:41:56 +0800

    fix: sidebar collapsible

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -37,7 +37,7 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
   };
 
   return (
-    <Sidebar {...props}>
+    <Sidebar collapsible="icon" {...props}>
       <SidebarHeader>
         <SidebarMenu>
           <SidebarMenuItem>
